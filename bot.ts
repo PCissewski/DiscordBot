ed758c59c8e99fa1684bb9a37317f991164216b9
PCissewski <cissek99@gmail.com>
2022-06-07 10:39:07 +0200

    switched to TypeScript bcs JS sucks

--- bot.ts
+++ bot.ts
@@ -1,5 +1,15 @@
+import { Message } from "discord.js";
+
 require('dotenv').config()
 const auth = require('./auth.json')
+
+const logger = require('winston')
+
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
 
 // Import necessary Discord.js classes
 const { Client, Intents } = require('discord.js');
@@ -21,7 +31,16 @@ client.on('ready', function(e){
 client.on('message',
     function(msg){
         msg.content = msg.content.toLowerCase();
-        if (msg.content === "arka gdynia"){
+        logger.info(msg.author.username);
+        if (msg.content[0] === "-") {
+            handleCommands(msg)
+        }
+        else if (msg.content === "arka gdynia"){
             msg.reply("kurwa świnia!")
         }
+        
     })
+
+function handleCommands(msg: Message){
+    msg.reply(msg.author.username)
+}
